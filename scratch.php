e9ff7895da0b28e2d20c569da58bbde09fb90c95
EMILY MONEY <130242169+emilymoney@users.noreply.github.com>
2023-04-09 19:57:48 -0400

    Add files via upload

---
+++ scratch.php
@@ -0,0 +1,13 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<link href="basic.css" type="text/css" rel="stylesheet"/>
+<head>
+    <meta charset="UTF-8">
+    <title>How-to-Play</title>
+</head>
+
